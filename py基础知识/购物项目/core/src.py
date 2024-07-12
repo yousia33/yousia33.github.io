6824fb138b2742b4019c73ebaabd73675e8376ca
yousia33 <63765497+yousia33@users.noreply.github.com>
2024-07-12 20:09:04 +0800

    7.12

---
+++ py基础知识/购物项目/core/src.py
@@ -0,0 +1,7 @@
+# 用户视图
+
+
+def run():
+    while True:
+        print()
+    pass
---
+++ py基础知识/购物项目/start.py
@@ -0,0 +1,11 @@
+# 程序入口
+
+import os
+import sys
+
+from core import src
+
+if __name__ =='__main__':
+    # 1. 用户视图层
+    
+    src.run()
