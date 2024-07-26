fdab1c1e11d7d9ad1fe5ae38c6c3df58808ad2d5
yousia33 <1853421303@qq.com>
2024-07-26 16:17:10 +0800

    7.26

--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
-class animal:
-    pass
-print('')
+class Riven:
+    camp = 'Noxus'
+    def __init__(self,nickname,aggressivity=54,life_value=414,money=1001,armor=3):
+        self.nickname=nickname
+        self.aggressivity=aggressivity
+        self.life_value
